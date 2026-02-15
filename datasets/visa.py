95693dcb462761bef63b5e3040053a4de38ddc3c
Dreamlights001 <3407844687@qq.com>
2026-02-16 03:42:13 +0800

    Update

--- datasets/visa.py
+++ datasets/visa.py
@@ -6,35 +6,51 @@ visa_classes = ['candle', 'capsules', 'cashew', 'chewinggum',
                    'fryum', 'macaroni1', 'macaroni2',
                 'pcb1', 'pcb2', 'pcb3','pcb4', 'pipe_fryum']
 
-VISA_DIR = '../datasets/VisA_pytorch/1cls'
+VISA_DIR = './datasets/visa'
 
 
 def load_visa(category, k_shot, experiment_indx):
-    def load_phase(root_path, gt_path):
+    def load_phase(img_path, mask_path=None):
         img_tot_paths = []
         gt_tot_paths = []
         tot_labels = []
         tot_types = []
 
-        defect_types = os.listdir(root_path)
+        if not os.path.exists(img_path):
+            return img_tot_paths, gt_tot_paths, tot_labels, tot_types
 
-        for defect_type in defect_types:
-            if defect_type == 'good':
-                img_paths = glob.glob(os.path.join(root_path, defect_type) + "/*.JPG")
+        for label_type in ['Normal', 'Anomaly']:
+            label_path = os.path.join(img_path, label_type)
+            if not os.path.exists(label_path):
+                continue
+
+            img_paths = glob.glob(os.path.join(label_path) + "/*.png")
+            img_paths.extend(glob.glob(os.path.join(label_path) + "/*.jpg"))
+            img_paths.extend(glob.glob(os.path.join(label_path) + "/*.JPG"))
+            img_paths.extend(glob.glob(os.path.join(label_path) + "/*.jpeg"))
+
+            if label_type == 'Normal':
                 img_tot_paths.extend(img_paths)
                 gt_tot_paths.extend([0] * len(img_paths))
                 tot_labels.extend([0] * len(img_paths))
                 tot_types.extend(['good'] * len(img_paths))
             else:
-                img_paths = glob.glob(os.path.join(root_path, defect_type) + "/*.JPG")
-                gt_paths = [os.path.join(gt_path, defect_type, os.path.basename(s)[:-4] + '.png') for s in
-                            img_paths]
-                img_paths.sort()
-                gt_paths.sort()
                 img_tot_paths.extend(img_paths)
-                gt_tot_paths.extend(gt_paths)
+                if mask_path is not None and os.path.exists(mask_path):
+                    for img_path_item in img_paths:
+                        img_name = os.path.basename(img_path_item)
+                        img_name_no_ext = img_name.rsplit('.', 1)[0]
+                        gt_files = glob.glob(os.path.join(mask_path, img_name_no_ext + ".*"))
+                        if not gt_files:
+                            gt_files = glob.glob(os.path.join(mask_path, img_name))
+                        if gt_files:
+                            gt_tot_paths.append(gt_files[0])
+                        else:
+                            gt_tot_paths.append(0)
+                else:
+                    gt_tot_paths.extend([0] * len(img_paths))
                 tot_labels.extend([1] * len(img_paths))
-                tot_types.extend([defect_type] * len(img_paths))
+                tot_types.extend(['anomaly'] * len(img_paths))
 
         assert len(img_tot_paths) == len(gt_tot_paths), "Something wrong with test and ground truth pair!"
 
@@ -44,28 +60,40 @@ def load_visa(category, k_shot, experiment_indx):
     assert k_shot in [0, 1, 5, 10]
     assert experiment_indx in [0, 1, 2]
 
-    test_img_path = os.path.join(VISA_DIR, category, 'test')
-    train_img_path = os.path.join(VISA_DIR, category, 'train')
-    ground_truth_path = os.path.join(VISA_DIR, category, 'ground_truth')
+    img_path = os.path.join(VISA_DIR, category, 'Data', 'Images')
+    mask_path = os.path.join(VISA_DIR, category, 'Data', 'Masks', 'Anomaly')
 
+    all_img_tot_paths, all_gt_tot_paths, all_tot_labels, all_tot_types = load_phase(img_path, mask_path)
 
-    train_img_tot_paths, train_gt_tot_paths, train_tot_labels, \
-    train_tot_types = load_phase(train_img_path, ground_truth_path)
+    random.seed(42)
+    combined = list(zip(all_img_tot_paths, all_gt_tot_paths, all_tot_labels, all_tot_types))
+    random.shuffle(combined)
+    all_img_tot_paths, all_gt_tot_paths, all_tot_labels, all_tot_types = zip(*combined)
+    all_img_tot_paths = list(all_img_tot_paths)
+    all_gt_tot_paths = list(all_gt_tot_paths)
+    all_tot_labels = list(all_tot_labels)
+    all_tot_types = list(all_tot_types)
 
-    test_img_tot_paths, test_gt_tot_paths, test_tot_labels, \
-    test_tot_types = load_phase(test_img_path, ground_truth_path)
+    split_idx = int(len(all_img_tot_paths) * 0.8)
+    train_img_tot_paths = all_img_tot_paths[:split_idx]
+    train_gt_tot_paths = all_gt_tot_paths[:split_idx]
+    train_tot_labels = all_tot_labels[:split_idx]
+    train_tot_types = all_tot_types[:split_idx]
+
+    test_img_tot_paths = all_img_tot_paths[split_idx:]
+    test_gt_tot_paths = all_gt_tot_paths[split_idx:]
+    test_tot_labels = all_tot_labels[split_idx:]
+    test_tot_types = all_tot_types[split_idx:]
 
     selected_train_img_tot_paths = []
     selected_train_gt_tot_paths = []
     selected_train_tot_labels = []
     selected_train_tot_types = []
 
-
     if k_shot > 0:
-        full_index = range(len(train_img_tot_paths))
-
         random.seed(42)
-        selected_index = random.sample(full_index, k_shot)
+        full_index = list(range(len(train_img_tot_paths)))
+        selected_index = random.sample(full_index, min(k_shot, len(full_index)))
         selected_train_img_tot_paths = [train_img_tot_paths[k] for k in selected_index]
         selected_train_gt_tot_paths = [train_gt_tot_paths[k] for k in selected_index]
         selected_train_tot_labels = [train_tot_labels[k] for k in selected_index]
